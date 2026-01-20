e1190de7870b4a89238698d0a872db0ca4823e5b
Neil Otniel Moreno Rivera <neilotniel@gmail.com>
2026-01-20 12:54:31 -0600

    Funcionalidad mapa con openmaps Leaflet

---
+++ public/js/map.js
@@ -0,0 +1,55 @@
+var map;
+var pin;
+var tilesURL = 'https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png';
+var mapAttrib = '';
+
+// 1. Esperar a que el HTML esté listo
+window.onload = function() {
+    MapCreate();
+    if (this.document.getElementById('map').exists) {       
+        document.getElementById('leaflet-control-attribution leaflet-control').hidden = true;
+    }
+};
+
+function MapCreate() {
+    // 2. Crear el contenedor si no existe
+    if (!document.getElementById('map')) {
+        var div = document.createElement('div');
+        div.id = 'map';
+        div.style.height = '100vh';
+        div.style.width = '80%';
+        div.style.marginLeft = 'auto';
+        document.body.prepend(div);
+    }
+
+    // 3. Inicializar el objeto map
+    map = L.map('map',{
+        attributionControl: false,
+        compass: true
+    }).setView([40, 0], 3);
+
+    L.tileLayer(tilesURL, {
+        attribution: mapAttrib,
+        maxZoom: 19
+    }).addTo(map);
+    // 4. Mover el listener de CLICK aquí adentro
+    // Esto garantiza que map ya está definido
+    map.on('click', function(ev) {
+        document.getElementById('lat').value = ev.latlng.lat;
+        document.getElementById('lng').value = ev.latlng.lng;
+
+        if (pin) {
+            pin.setLatLng(ev.latlng);
+        } else {
+            pin = L.marker(ev.latlng, { riseOnHover: true, draggable: true }).addTo(map);
+            
+            // Evento drag corregido (e.target)
+            pin.on('drag', function(e) {
+                var position = e.target.getLatLng();
+                document.getElementById('lat').value = position.lat;
+                document.getElementById('lng').value = position.lng;
+            });
+        }
+    });     
+    
+}
