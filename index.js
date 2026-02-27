6dd1573560d16ce10efc7107e152bd7a7e1e4376
Neil Otniel Moreno Rivera <neilotniel@gmail.com>
2026-02-27 09:49:27 -0600

    commit para recordar subir el docker compuse

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3306;
 const cors= require('cors');
 const fs = require('fs');
 app.use(cors()); // Enable CORS for all routes
